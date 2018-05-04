21e5a18cdfdeed456f28e1c1eb03ab1cf881af55
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-05-04 14:49:10 -0700

    js

---
+++ assets/javascript/nyt-search.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function() {
+
+  var API_KEY = "ec31f6bbf356401ca9ac776ffb8a42fc"
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+
+  // $('.form-group').each(function() {
+  //   $(this).attr('data-' + id,'PickleRick');
+  // })
+
+  $('#searchTerm').attr('data-term','searchTerm');
+
+
+  queryURL += $.param({
+    'api-key': API_KEY,
+    'q': 'Terrorists'
+    // 'begin_date': null,
+    // 'end_date': null
+  })
+
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function(response) {
+    console.log(response);
+  })
+
+})
