110cf63d6d7bb416dcdb346304945990a452582f
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-05-04 16:38:12 -0700

    Add logic for click event

--- assets/javascript/nyt-search.js
+++ assets/javascript/nyt-search.js
@@ -1,32 +1,68 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-  var API_KEY = "ec31f6bbf356401ca9ac776ffb8a42fc";
-  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
-  
+  function nytSearch(queryObj) {
 
-  // $('.form-group’).each(function() {
-  //   $(this).attr(‘data-’ + id,‘PickleRick’);
-  // })
+    console.log(queryObj);
 
-  $('#searchTerm').attr('data-term','searchTerm');
-  $('#numOfRecords').attr('data-term','records');
-  $('#startYear').attr('data-term','startYear');
-  $('#endYear').attr('data-term','endYear');
+    var API_KEY = "ec31f6bbf356401ca9ac776ffb8a42fc";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
 
+    // $('.form-group’).each(function() {
+    //   $(this).attr(‘data-’ + id,‘PickleRick’);
+    // })
 
-  queryURL += $.param({
-    'api-key': API_KEY,
-    'q': 'Terrorists'
-    // 'begin_date': null,
-    // 'end_date': null
-  })
+    $('#searchTerm').attr('data-term', 'searchTerm');
+    $('#numOfRecords').attr('data-term', 'records');
+    $('#startYear').attr('data-term', 'startYear');
+    $('#endYear').attr('data-term', 'endYear');
 
-  $.ajax({
-    url: queryURL,
-    method: 'GET'
-  }).then(function(response) {
-    console.log(response);
-  })
+    var usedByParam = {};
+    usedByParam['api-key'] = queryObj.searchTerm;
+    usedByParam['q'] = queryObj.searchTerm;
+    
+    if(queryObj.startYear) {
+      usedByParam['begin_date'] = queryObj.startYear;
+    }
+
+    if(queryObj.endYear) {
+      usedByParam['end_date'] = queryObj.endYear;
+    }
+
+    console.log(usedByParam);
+    
+    // queryURL += $.param(usedByParam);
+
+    // $.ajax({
+    //   url: queryURL,
+    //   method: 'GET'
+    // }).then(function (response) {
+    //   console.log('Then!')
+    //   console.log(response);
+    // })
+  }
+
+  $("#search").on("click", function (event) {
+    event.preventDefault();
+    console.log($('#startYear').val().length);
+    console.log($('#endYear').val())
+    var startDate = $('#startYear').val().replace(/-/g, "");
+    var endDate = $('#endYear').val().replace(/-/g, "");
+
+    var queryObj = {};
+
+    if(!startDate.length === 0) {
+      queryObj.startYear = startDate;
+    } 
+    
+    if(!endDate.legnth === 0) {
+      queryObj.endYear = endDate;
+    }
+
+    queryObj.searchTerm = $("#searchTerm").val().trim();
+    queryObj.numOfRecords = $('#numOfRecords').val();
+
+    nytSearch(queryObj);
+  });
 
 });
